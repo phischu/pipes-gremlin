d919bb1ed87c28ac23a851617ad0214fd0bd5565
Philipp Schuster <pschuster@uni-koblenz.de>
2013-05-25 20:00:48 +0200

    crazy attempt to define aggregate oh boy

--- src/Database/PipesGremlin.hs
+++ src/Database/PipesGremlin.hs
@@ -1,7 +1,9 @@
-{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveDataTypeable, ScopedTypeVariables #-}
 module Database.PipesGremlin where
 
-import Control.Proxy (Proxy,ProduceT,RespondT(RespondT),Pipe,request,respond,mapD,(>->),eachS)
+import Control.Proxy (runProxy,printD,fromListS,foreverK,Proxy,ProduceT,RespondT(RespondT,runRespondT),Pipe,request,respond,mapD,(>->),eachS,toListD,liftP,ProxyFast)
+import Control.Proxy.Trans.Writer (WriterP,execWriterK)
+import Control.Proxy.Trans.Identity (runIdentityK)
 import Control.Proxy.Safe (ExceptionP,SafeIO,tryIO,throw)
 
 import Database.Neo4j (
@@ -9,7 +11,7 @@ import Database.Neo4j (
     Relationship,RelationshipType,relationshipType,outgoingRelationships,
     relationshipTo)
 
-import Control.Monad (forever,liftM,(>=>))
+import Control.Monad (forever,liftM,(>=>),replicateM_)
 import Control.Exception (Exception)
 import Data.Typeable (Typeable)
 
@@ -32,6 +34,27 @@ outEdges node = RespondT (do
 
 inVertices :: (Proxy p,Monad m) => Relationship -> ProduceT p m Node
 inVertices = return . relationshipTo
+
+aggregate :: (Proxy p,Monad m) => (() -> RespondT p () a () m b) -> () -> RespondT p () a () m [b]
+aggregate p () = undefined
+
+
+x :: Pipe p a b m r -> Pipe p a [b] m r
+x = undefined
+
+whatever p = foreverK ((execWriterK (liftP . p >-> toListD >-> exhaust)) >=> respond)
+
+yoyo = runProxy (runIdentityK (fromListS [1,2,3] >-> whatever f >-> printD))
+
+exhaust () = forever (do
+	request ())
+
+f () = do
+	x <- request ()
+	replicateM_ x (respond (show x))
+
+scatter :: (Proxy p,Monad m) => [a] -> ProduceT p m a
+scatter = eachS
 
 data PipesGremlinError = PipesGremlinError String deriving (Show,Typeable)
 
